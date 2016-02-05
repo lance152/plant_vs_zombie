bef587e2297ae4be132cfe6a6a0df051e3216b28
Runzhou Fan <runzhoufan@cmail.carleton.ca>
2016-02-05 15:36:08 -0500

    11

---
+++ plant vs zombie test/plant vs zombie test/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace plant_vs_zombie_test
+{
+    class Garden
+    {
+        public int GARDEN_LENGTH = 6;
+        public int GARDEN_WIDTH = 3;
+        public int INITIAL_SUNS = 50;
+        private int suns;
+        private ArrayList sunflowers;
+
+        public Garden()
+        {
+            suns = INITIAL_SUNS;
+            sunflowers = new ArrayList();
+
+        }
+
+        public void addSuns(int suns)
+        {
+            this.suns += suns;
+        }
+
+        public void subtractSuns(int suns)
+        {
+            this.suns -= suns;
+        }
+
+        public void addSunflower(Sunflower sf)
+        {
+            sunflowers.Add(sf);
+        }
+
+        public void increment()
+        {
+            foreach (Sunflower sf in sunflowers)
+            {
+                sf.increment();
+            }
+        }
+
+        public String toString()
+        {
+            String s = "";
+            for(int a = 0; a < GARDEN_WIDTH; a++)
+            {
+                for (int b = 0; b < GARDEN_LENGTH; b++)
+                {
+                    if (hasSunflowerAt(b,a))
+                    {
+                        s += "S";
+                    }
+                    else
+                    {
+                        s += ".";
+                    }
+                }
+                s += "\n";
+            }
+            s += "suns = " + suns + "\n";
+            return s;
+        }
+
+        public void print()
+        {
+            Console.Write(this.toString());
+        }
+
+        private bool hasSunflowerAt(int x, int y)
+        {
+            foreach (Sunflower sf in sunflowers)
+            {
+                if (sf.getX() == x && sf.getY() == y)
+                {
+                    return true;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            Garden g = new Garden();
+            Sunflower sf = new Sunflower(g, 1, 0);
+            g.print();
+            g.increment();
+            g.increment();
+            g.increment();
+            g.print();
+
+        }
+    }
+}
---
+++ plant vs zombie test/plant vs zombie test/Sunflower.cs
@@ -0,0 +1,46 @@
+﻿namespace plant_vs_zombie_test
+{
+    internal class Sunflower
+    {
+        public int COST = 50;
+        public int YIELD = 20;
+        public int PRODUCTION_CYCLE = 3;
+        private Garden garden;
+        private int positionInCYcle;
+        private int x, y;
+
+        public Sunflower(Garden g, int x, int y)
+        {
+            garden = g;
+            this.x = x;
+            this.y = y;
+            positionInCYcle = 0;
+            g.addSunflower(this);
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+
+        public void increment()
+        {
+            positionInCYcle++;
+            if(positionInCYcle % 3 == 0)
+            {
+                this.produceSuns();
+                positionInCYcle = 0;
+            }
+        }
+
+        private void produceSuns()
+        {
+            garden.addSuns(YIELD);
+        }
+    }
+}
